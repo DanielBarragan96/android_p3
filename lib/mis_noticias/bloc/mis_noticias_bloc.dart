f02073f0fb775dbb93edab138c198e058e59bab9
DanielBarragan96 <ie702554@iteso.mx>
2020-11-06 09:03:47 -0600

    implementing multiple methods for mis noticias

--- lib/mis_noticias/bloc/mis_noticias_bloc.dart
+++ lib/mis_noticias/bloc/mis_noticias_bloc.dart
@@ -1,16 +1,22 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:noticias/models/noticia.dart';
 import 'package:path/path.dart' as Path;
 
 part 'mis_noticias_event.dart';
 part 'mis_noticias_state.dart';
 
 class MisNoticiasBloc extends Bloc<MisNoticiasEvent, MisNoticiasState> {
+  List<Noticia> _noticiasList;
+  File _choosenImage;
+  List<Noticia> get getNoticiasList => _noticiasList;
+
   MisNoticiasBloc() : super(MisNoticiasInitial());
 
   @override
@@ -18,14 +24,30 @@ class MisNoticiasBloc extends Bloc<MisNoticiasEvent, MisNoticiasState> {
     MisNoticiasEvent event,
   ) async* {
     if (event is CrearNoticiaEvent) {
-      //
+      try {
+        String imageUrl = await _uploadPicture(_choosenImage);
+        await _saveNoticia(
+          event.title,
+          event.description,
+          event.autor,
+          event.fuente,
+          imageUrl,
+        );
+        yield NoticiasCreadaState();
+      } catch (e) {}
     } else if (event is LeerNoticiasEvent) {
-      //
+      try {
+        await _getAllNoticias();
+      } catch (e) {
+        yield NoticiasErrorState(errorMsg: "No se pudo cargar noticia: $e");
+      }
     } else if (event is CargarImagenEvent) {
-      _chooseImage(event.takePictureFromCamera);
+      _choosenImage = await _chooseImage(event.takePictureFromCamera);
+      yield ImagenCargadaState(imagen: _choosenImage);
     }
   }
 
+  // subir imagen al bucket de almacenamiento
   Future<String> _uploadPicture(File image) async {
     String imagePath = image.path;
     // referencia al storage de firebase
@@ -51,5 +73,46 @@ class MisNoticiasBloc extends Bloc<MisNoticiasEvent, MisNoticiasState> {
       imageQuality: 85,
     );
     return File(chooseImage.path);
+  }
+
+  Future _getAllNoticias() async {
+    // recuperar lista de docs guardados en Cloud firestore
+    // mapear a objeto de dart (Apunte)
+    // agregar cada ojeto a una lista
+
+    var misNoticias =
+        await FirebaseFirestore.instance.collection("noticias").get();
+
+    _noticiasList = misNoticias.docs
+        .map(
+          (elemento) => Noticia(
+            title: elemento["titulo"],
+            description: elemento["description"],
+            author: elemento["autor"],
+            source: elemento["fuente"],
+            urlToImage: elemento["imagen"],
+            publishedAt: elemento["fecha"],
+          ),
+        )
+        .toList();
+  }
+
+  // guardar en CloudFirestore
+  Future _saveNoticia(
+    String title,
+    String description,
+    String autor,
+    String fuente,
+    String imageUrl,
+  ) async {
+    // Crea un doc en la collection de apuntes
+    await FirebaseFirestore.instance.collection("noticias").doc().set({
+      "title": title,
+      "description": description,
+      "autor": autor,
+      "fuente": fuente,
+      "imagen": imageUrl,
+      "fecha": DateTime.now().toString(),
+    });
   }
 }
